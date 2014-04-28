6f1ad3a28957d0afdd52a816b841f0847e368d35
Ken Tominaga <ken.for.develop@gmail.com>
2014-04-28 12:03:21 +0900

    Complete task08

--- 140414/task08.el
+++ 140414/task08.el
@@ -3,6 +3,7 @@
   (if (good-enough? guess x)
       guess
     (sqrt-iter (improve guess x) x)))
-(defun good-enough? (guess diff) (< (abs (- (square guess) diff)) 0.001))
+(defun good-enough? (guess x) (< (abs (- (square guess) x)) 0.001))
+(defun good-enough? (guess x) (< (abs (- guess (improve guess x))) 0.00001))
 (defun improve (guess x) (/ (+ guess (/ x guess)) 2))
 (defun my-sqrt (x) (sqrt-iter 1.0 x))

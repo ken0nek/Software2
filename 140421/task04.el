3cd64a009d9781960a8b2e1d98ecb50ffec05c86
Ken Tominaga <ken.for.develop@gmail.com>
2014-05-19 01:44:00 +0900

    Complete task04

--- 140421/task04.el
+++ 140421/task04.el
@@ -1,4 +1,6 @@
-(defun make-rat (n d) (cons n d))
+(defun make-rat (n d)
+	(let ((c (gcd n d)))
+		(cons (/ n c) (/ d c))))
 
 (defun numer (x) (car x))
 
@@ -10,3 +12,7 @@
 	(* (denom x) (denom y))))
 
 (defun print-rat (x) (prin1 (numer x))(prin1 '/)(prin1 (denom x))(print nil))
+
+(setq one-third (make-rat 1 3))
+
+(print-rat (add-rat one-third one-third))

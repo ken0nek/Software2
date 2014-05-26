caa4720151fd53a72d9d8f724ea9da003e5ce93e
Ken Tominaga <ken.for.develop@gmail.com>
2014-05-26 11:51:18 +0900

    Complete student_h.h

---
+++ 140512/student_h.h
@@ -0,0 +1,25 @@
+class Student {
+private:
+	char name[32];
+	int grade;
+
+public:
+	Student(const char* name) {
+		strcpy(name, _name);
+		grade = 0;
+	}
+	~Student() {
+	}
+
+	void SetGrade(int _grade) {
+		grade = _grade;
+	}
+
+	int GetGrade() {
+		return grade;
+	}
+
+	char* GetName() {
+		return name;
+	}
+};
